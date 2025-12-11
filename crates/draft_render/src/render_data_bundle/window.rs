56c542053299b9526297de4a075e41d90406a7fe
zuiyu1998 <1542844298@qq.com>
2025-12-11 13:40:28 +0800

    refactor: refactor RenderWindows

--- crates/draft_render/src/render_data_bundle/window.rs
+++ crates/draft_render/src/render_data_bundle/window.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use frame_graph::gfx_base::{GpuSurfaceTexture, TextureView};
 
 pub struct RenderWindow {
@@ -5,10 +7,16 @@ pub struct RenderWindow {
     pub surface_texture_view: TextureView,
 }
 
-pub struct RenderWindows(RenderWindow);
+#[derive(Debug, PartialEq, Eq, Hash)]
+pub struct WindowId(usize);
+
+pub struct RenderWindows {
+    primary: Option<WindowId>,
+    data: HashMap<WindowId, RenderWindow>,
+}
 
 impl RenderWindows {
-    pub fn primary(&self) -> &RenderWindow {
-        &self.0
+    pub fn primary(&self) -> Option<&RenderWindow> {
+        self.primary.as_ref().and_then(|id| self.data.get(id))
     }
 }
