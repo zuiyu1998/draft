c4bc40b2f33bcd6dcfa3baf5689b33ed93cdda8a
zuiyu1998 <1542844298@qq.com>
2026-03-31 14:05:54 +0800

    feat: add Mesh2d for demo

--- examples/demo.rs
+++ examples/demo.rs
@@ -1,7 +1,17 @@
-use draft_app::app::{App, Executor, WinitExecutor};
+use draft_app::{
+    app::{App, Executor, WinitExecutor},
+    scene::{Mesh2d, Node, SceneNode},
+};
 
 fn main() {
-    let app = App::empty();
+    let mut app = App::empty();
+
+    let mesh2d = Mesh2d {
+        node: Node::empty(),
+        surfaces: Default::default(),
+    };
+
+    app.scene.graph.add_node(SceneNode::new(mesh2d));
 
     let mut executor = WinitExecutor::default();
     executor.run(app);
