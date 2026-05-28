d873cf25732c0a329120773494568bcd59e4b3c4
zuiyu1998 <1542844298@qq.com>
2026-05-28 10:00:28 +0800

    feat: add Renderer2d

--- crates/draft_render/src/lib.rs
+++ crates/draft_render/src/lib.rs
@@ -1,11 +1,8 @@
 pub mod error;
 pub mod frame_graph;
-pub mod render_command;
 pub mod render_pipeline;
 pub mod render_world;
-pub mod renderer;
-
-use std::mem::take;
+pub mod renderer_2d;
 
 use draft_graphics::RenderServer;
 use draft_window::SystemWindowManager;
@@ -13,10 +10,8 @@ use draft_window::SystemWindowManager;
 pub const CORE_2D: &str = "core_2d";
 
 use crate::{
-    render_command::{RenderCommandContext, RenderCommandsContainer},
     render_pipeline::{RenderPipeline, RenderPipelineContainer, RenderPipelineRunContext},
     render_world::RenderWorld,
-    renderer::{MeshMaterialRenderer2d, MeshMaterialRendererContainer},
 };
 
 pub use error::FrameworkError;
@@ -27,7 +22,6 @@ pub trait IWorld: 'static {
 
 pub struct RenderContext<'a> {
     pub render_world: &'a mut RenderWorld,
-    pub render_commands_container: &'a mut RenderCommandsContainer,
 }
 
 pub struct WorldRenderer {
@@ -35,8 +29,6 @@ pub struct WorldRenderer {
     pub system_window_manager: SystemWindowManager,
     pub render_pipeline_container: RenderPipelineContainer,
     pub render_world: RenderWorld,
-    pub render_commands_container: RenderCommandsContainer,
-    pub mesh_material_renderer_container: MeshMaterialRendererContainer,
 }
 
 impl WorldRenderer {
@@ -46,32 +38,12 @@ impl WorldRenderer {
             render_server,
             system_window_manager,
             render_pipeline_container: RenderPipelineContainer::default(),
-            render_commands_container: RenderCommandsContainer::new(),
-            mesh_material_renderer_container: MeshMaterialRendererContainer::default(),
         }
     }
 
     pub fn initialize(&mut self) {
         self.render_pipeline_container
             .insert(CORE_2D, RenderPipeline::default());
-
-        self.mesh_material_renderer_container
-            .insert(CORE_2D, MeshMaterialRenderer2d {});
-    }
-
-    fn flush(&mut self) {
-        let mut render_commands_container = take(&mut self.render_commands_container);
-
-        for (key, mesh_material_renderer) in self.mesh_material_renderer_container.iter_mut() {
-            let mut context = RenderCommandContext {
-                render_world: &mut self.render_world,
-                mesh_material_renderer: mesh_material_renderer.as_mut(),
-            };
-
-            let render_commands = render_commands_container.get_or_insert(&key);
-
-            render_commands.execute(&mut context);
-        }
     }
 
     pub fn render<W: IWorld>(&mut self, world: &W) {
@@ -80,12 +52,9 @@ impl WorldRenderer {
 
         let mut context = RenderContext {
             render_world: &mut self.render_world,
-            render_commands_container: &mut self.render_commands_container,
         };
 
         world.render(&mut context);
-
-        self.flush();
 
         let mut context = RenderPipelineRunContext {};
 
---
+++ crates/draft_render/src/renderer_2d.rs
@@ -0,0 +1,7 @@
+pub struct Renderer2d {}
+
+impl Default for Renderer2d {
+    fn default() -> Self {
+        Self {}
+    }
+}
